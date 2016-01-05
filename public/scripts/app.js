3d12ac76a4a7fbf55de59ed8d399a4fca244cfd9
lduboeuf <lduboeuf@ouvaton.org>
2016-01-05 09:32:27 +0100

    move resources and routes as a separate files

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -11,16 +11,6 @@ angular.module('mytodoApp', [
   'ui.bootstrap',
   'dialogs.main' //https://github.com/m-e-conroy/angular-dialog-service
 ])
-  .config(['$routeProvider', function($routeProvider){
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/todos.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  }])
   .config(['$resourceProvider', function($resourceProvider) {
     // this is to allow calling GET /todos/ instead of /todos
     $resourceProvider.defaults.stripTrailingSlashes = false;
@@ -39,13 +29,6 @@ angular.module('mytodoApp', [
     });
 
   }])
-  .factory('Todo', ['$resource', function($resource){
-      return $resource('/api/todos/:id', {id:'@id'}, {
-        update: {
-          method: 'PUT' // this method issues a PUT request
-        }
-      });
-    }])
   .run(function(editableOptions) {
       editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
     });
